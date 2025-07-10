59c9323792a31b42376b126bdf0686c2344800b3
Davidson Santos Conceição <davidson.cabista@gmail.com>
2025-07-10 15:37:03 +0000

    Para concluir este guia, verifique se o ambiente de desenvolvimento aten

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,9 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import { firebase } from '@genkit-ai/firebase/plugin';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    firebase(), // Adiciona o plugin do Firebase para autenticação no servidor
   ],
   model: 'googleai/gemini-2.0-flash',
 });
