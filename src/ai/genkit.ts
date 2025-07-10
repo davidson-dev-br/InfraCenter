17c56caad721590c60d0ee63bf53165941bf1df0
Davidson Santos Conceição <davidson.cabista@gmail.com>
2025-07-10 15:19:45 +0000

    eu imaginei que quando eu estivece fazendo uma leitura com IA ela tenta

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,12 @@
+'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import { enableFirebaseTelemetry } from '@genkit-ai/firebase';
-
-enableFirebaseTelemetry();
+import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI(),
+    firebase(), // Adiciona o plugin do Firebase para autenticação no servidor
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
