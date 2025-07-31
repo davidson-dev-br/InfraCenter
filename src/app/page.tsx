0029058618c166ba97305d96e7b5adce7dcf5bce
Davidson Santos Conceição <davidson.cabista@gmail.com>
2025-07-31 18:37:22 +0000

    a pagina que previsualiza o projeto aqui não esta aparecendo cmo arrumar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Skeleton } from "@/components/ui/skeleton";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   const router = useRouter();
 
